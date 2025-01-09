9d6061e2f97020fa08a532b0f4afe359e1e01746
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-01-09 09:02:01 +0700

    FIX: design icon footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -125,7 +125,7 @@ const FooterComponent = () => {
               href="#"
               className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
               <svg
-                className="w-4 h-4"
+                className="w-6 h-6"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="currentColor"
@@ -142,7 +142,7 @@ const FooterComponent = () => {
               href="#"
               className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
               <svg
-                className="w-4 h-4"
+                className="w-6 h-6"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="currentColor"
@@ -155,7 +155,7 @@ const FooterComponent = () => {
               href="#"
               className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
               <svg
-                className="w-4 h-4"
+                className="w-6 h-6"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="currentColor"
@@ -172,7 +172,7 @@ const FooterComponent = () => {
               href="#"
               className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
               <svg
-                className="w-4 h-4"
+                className="w-6 h-6"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="currentColor"
