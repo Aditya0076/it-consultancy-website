9c93b96be41fe149cfd95f6d5bf7fe802c6141b2
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-01-09 16:34:48 +0700

    Add custom gradient

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,22 @@ const flowbite = require("flowbite-react/tailwind");
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}",flowbite.content(),],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}", flowbite.content()],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        customGradient: `
+          linear-gradient(to bottom, rgba(255,255,255,0.15) 0%, rgba(0,0,0,0.15) 100%),
+          radial-gradient(at top center, rgba(255,255,255,0.40) 0%, rgba(0,0,0,0.40) 120%)
+        `,
+      },
+      backgroundBlendMode: {
+        multiply: "multiply",
+      },
+      colors: {
+        "custom-gray": "#989898",
+      },
+    },
   },
-  plugins: [
-   flowbite.plugin(),
-  ],
+  plugins: [flowbite.plugin()],
 };
