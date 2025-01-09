03f07460f36fa5c3999f5f73efe4f72f8ddf40d5
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-01-09 10:55:18 +0700

    FIX: Responsive Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = ({ toggleDarkMode, isDarkMode }) => {
 
   return (
     <nav className={`bg-white border-b-2 border-gray-200 dark:bg-gray-900`}>
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-8">
+      <div className="max-w-screen-xl flex md:flex-wrap gap-6 items-center justify-between mx-auto p-8">
         <NavLink
           to="/"
           className="flex items-center justify-center w-full md:w-auto">
